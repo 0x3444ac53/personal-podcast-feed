d473322fd7f5f5b111a673bf159783623c13a9d4
Tanner Galyean <sprinklelong@gmail.com>
2018-05-05 15:07:15 -0400

    maybe working pushing to server

--- audio-bot.py
+++ audio-bot.py
@@ -10,8 +10,7 @@ import json
 import key
 global api
 import os
-
-myurl = 'http://67.205.175.194:5745/'
+import datetime
 
 
 class StdOutListener(StreamListener):
@@ -45,6 +44,42 @@ class StdOutListener(StreamListener):
         }
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([urll])
+        self.gereate_feed()
+
+    def gereate_feed(self):
+        pubtime = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
+        files = os.listdir('./')
+        for i in files:
+            if '.mp3' not in i:
+                files.pop(files.index(i))
+        with open('feed.rss', 'w') as f:
+            f.truncate()
+            f.write("""
+                <?xml version="1.0" encoding="utf-8"?>
+                <rss version="2.0" xmlns:itunes="http://www.itunes.com/DTDs/Podcast-1.0.dtd">
+
+                <channel>
+                <title> Tanners Podcast </title>
+                <description> ???? </description>
+                <itunes:author>Tanner Galyean </itunes:author>
+                <link> http://104.131.56.81/ </link>
+                <itunes:image href="podcast_logo_grey.png" />
+                <pubDate> Sun, 09 Oct 2005 21:00:00 PST </pubDate>
+                <language>en-us</language>
+                <copyright> 1997 tanner galyean </copyright>
+                    """)
+            for i in files:
+                f.write("""
+                    <item>
+                    <title> {} </title>
+                    <description> A description of your podcast episode </description>
+                    <itunes:author> Tanner Galyean </itunes:author>
+                    <pubDate> {} </pubDate>
+                    <enclosure url="http://104.131.56.81/" + {} length={} type="audio/mpeg" /> </item>
+                    """.format(
+                    i.strip('.mp3'), pubtime, i,
+                    os.stat(i).st_size))
+            f.write("</channel></rss>")
 
 
 def start():
