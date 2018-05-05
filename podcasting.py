2a89bfb99805f9f9a971be7d6332e4ddeff0ff7d
Tanner Galyean <sprinklelong@gmail.com>
2018-05-05 03:25:32 -0400

    fixed typo

--- podcasting.py
+++ podcasting.py
@@ -87,30 +87,21 @@ def getFiles(dirname, extensions=None, recursive=False):
     allFiles = []
     if recursive:
         for root, dirs, filenames in os.walk(dirname):
-            for name in filenames:
-                allFiles.append(os.path.join(root, name))
+                for name in filenames:
+                    allFiles.append(os.path.join(root, name))
     else:
         allFiles = [f for f in glob.glob(dirname + "*") if os.path.isfile(f)]
 
     if extensions is not None:
         for ext in set([e.lower() for e in extensions]):
-            selectedFiles += [
-                n for n in allFiles
-                if fnmatch.fnmatch(n.lower(), "*{0}".format(ext))
-            ]
+            selectedFiles += [n for n in allFiles if fnmatch.fnmatch(n.lower(), "*{0}".format(ext))]
     else:
         selectedFiles = allFiles
 
     return sorted(set(selectedFiles))
 
 
-def buildItem(link,
-              title,
-              guid=None,
-              description="",
-              pubDate=None,
-              indent="   ",
-              extraTags=None):
+def buildItem(link, title, guid = None, description="", pubDate=None, indent = "   ", extraTags=None):
     '''
     Generate a RSS 2 item and return it as a string.
 
@@ -226,11 +217,10 @@ def buildItem(link,
     if guid is None:
         guid = link
 
-    guid = "{0}<guid>{1}</guid>\n".format(indent * 3, guid)
+    guid =  "{0}<guid>{1}</guid>\n".format(indent * 3, guid)
     link = "{0}<link>{1}</link>\n".format(indent * 3, link)
     title = "{0}<title>{1}</title>\n".format(indent * 3, title)
-    descrption = "{0}<description>{1}</description>\n".format(
-        indent * 3, description)
+    descrption = "{0}<description>{1}</description>\n".format(indent * 3, description)
 
     if pubDate is not None:
         pubDate = "{0}<pubDate>{1}</pubDate>\n".format(indent * 3, pubDate)
@@ -247,18 +237,17 @@ def buildItem(link,
             value = tag.get("value", None)
             params = tag.get("params", '')
             if params is None:
-                params = ''
+               params = ''
             if isinstance(params, (list)):
-                params = " ".join(params)
+               params = " ".join(params)
             if len(params) > 0:
-                params = " " + params
+               params = " " + params
 
             extra += "{0}<{1}{2}".format(indent * 3, name, params)
-            extra += "{0}\n".format("/>" if value is None else
-                                    ">{0}</{1}>".format(value, name))
+            extra += "{0}\n".format("/>" if value is None else ">{0}</{1}>".format(value, name))
 
-    return "{0}<item>\n{1}{2}{3}{4}{5}{6}{0}</item>".format(
-        indent * 2, guid, link, title, descrption, pubDate, extra)
+    return "{0}<item>\n{1}{2}{3}{4}{5}{6}{0}</item>".format(indent * 2, guid, link, title,
+                                                            descrption, pubDate, extra)
 
 
 def fileToItem(host, fname, pubDate):
@@ -313,21 +302,15 @@ def fileToItem(host, fname, pubDate):
     fileURL = urllib.quote(host + fname.replace("\\", "/"), ":/")
     fileMimeType = mimetypes.guess_type(fname)[0]
 
-    if fileMimeType is not None and ("audio" in fileMimeType
-                                     or "video" in fileMimeType):
-        tagParams = "url=\"{0}\" type=\"{1}\" length=\"{2}\"".format(
-            fileURL, fileMimeType, os.path.getsize(fname))
-        enclosure = {"name": "enclosure", "value": None, "params": tagParams}
+    if fileMimeType is not None and ("audio" in fileMimeType or "video" in fileMimeType):
+        tagParams = "url=\"{0}\" type=\"{1}\" length=\"{2}\"".format(fileURL, fileMimeType, os.path.getsize(fname))
+        enclosure = {"name" : "enclosure", "value" : None, "params": tagParams}
     else:
         enclosure = None
 
-    return buildItem(
-        link=fileURL,
-        title=os.path.basename(fname),
-        guid=fileURL,
-        description=os.path.basename(fname),
-        pubDate=pubDate,
-        extraTags=[enclosure])
+    return buildItem(link=fileURL, title=os.path.basename(fname),
+                     guid=fileURL, description=os.path.basename(fname),
+                     pubDate=pubDate, extraTags=[enclosure])
 
 
 def main(argv=None):
@@ -336,8 +319,7 @@ def main(argv=None):
     program_version = "v0.1"
     program_build_date = "%s" % __updated__
 
-    program_version_string = '%%prog %s (%s)' % (program_version,
-                                                 program_build_date)
+    program_version_string = '%%prog %s (%s)' % (program_version, program_build_date)
     program_usage = "genRSS -d directory [OPTIONS]"
     program_longdesc = "Generates an RSS feed from files in a directory"
     program_license = "Copyright 2014-2017 Amine SEHILI. Licensed under the MIT License"
@@ -345,108 +327,56 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
     try:
-        parser = argparse.ArgumentParser(
-            usage=program_usage,
-            description=program_longdesc,
-            formatter_class=argparse.RawTextHelpFormatter)
-        parser.add_argument(
-            "-d",
-            "--dirname",
-            dest="dirname",
-            help="Directory to look for media files in.\n"
-            "This directory name will be appended to the host name\n"
-            "to create absolute paths to your media files.",
-            metavar="DIRECTORY")
-        parser.add_argument(
-            "-r",
-            "--recursive",
-            dest="recursive",
-            help="Look for media files recursively in sub directories\n"
-            "[Default:False]",
-            action="store_true",
-            default=False)
+        parser = argparse.ArgumentParser(usage=program_usage, description=program_longdesc,
+                                         formatter_class=argparse.RawTextHelpFormatter)
+        parser.add_argument("-d", "--dirname", dest="dirname",
+                           help="Directory to look for media files in.\n"
+                                "This directory name will be appended to the host name\n"
+                                "to create absolute paths to your media files.",
+                           metavar="DIRECTORY")
+        parser.add_argument("-r", "--recursive", dest="recursive",
+                          help="Look for media files recursively in sub directories\n"
+                               "[Default:False]",
+                          action="store_true", default=False)
 
-        parser.add_argument(
-            "-e",
-            "--extensions",
-            dest="extensions",
-            help=
-            "A comma separated list of extensions (e.g. mp3,mp4,avi,ogg)\n[Default: all files]",
-            type=str,
-            default=None,
-            metavar="STRING")
+        parser.add_argument("-e", "--extensions", dest="extensions",
+                          help="A comma separated list of extensions (e.g. mp3,mp4,avi,ogg)\n[Default: all files]",
+                          type=str, default=None, metavar="STRING")
 
-        parser.add_argument(
-            "-o",
-            "--out",
-            dest="outfile",
-            help="Output RSS file [default: stdout]",
-            metavar="FILE")
-        parser.add_argument(
-            "-H",
-            "--host",
-            dest="host",
-            help="Host name (or IP address), possibly with a protocol\n"
-            "(default: http) a port number and the path to the base\n"
-            "directory where your media directory is located.\n"
-            "Examples of host names:\n"
-            " - http://localhost:8080 [default]\n"
-            " - mywebsite.com/media/JapaneseLessons\n"
-            " - mywebsite\n"
-            " - 192.168.1.12:8080\n"
-            " - http://192.168.1.12/media/JapaneseLessons\n",
-            default="http://localhost:8080",
-            metavar="URL")
-        parser.add_argument(
-            "-i",
-            "--image",
-            dest="image",
-            help="Absolute or relative URL for feed's image [default: None]",
-            default=None,
-            metavar="URL")
+        parser.add_argument("-o", "--out", dest="outfile", help="Output RSS file [default: stdout]", metavar=   "FILE")
+        parser.add_argument("-H", "--host", dest="host", help="Host name (or IP address), possibly with a protocol\n"
+                                                              "(default: http) a port number and the path to the base\n"
+                                                              "directory where your media directory is located.\n"
+                                                              "Examples of host names:\n"
+                                                              " - http://localhost:8080 [default]\n"
+                                                              " - mywebsite.com/media/JapaneseLessons\n"
+                                                              " - mywebsite\n"
+                                                              " - 192.168.1.12:8080\n"
+                                                              " - http://192.168.1.12/media/JapaneseLessons\n",
+                            default="http://localhost:8080",  metavar="URL")
+        parser.add_argument("-i", "--image", dest="image",
+                          help="Absolute or relative URL for feed's image [default: None]",
+                          default = None, metavar="URL")
 
-        parser.add_argument(
-            "-t",
-            "--title",
-            dest="title",
-            help="Title of the podcast [Defaule:None]",
-            default=None,
-            metavar="STRING")
-        parser.add_argument(
-            "-p",
-            "--description",
-            dest="description",
-            help="Description of the podcast [Defaule:None]",
-            default=None,
-            metavar="STRING")
-        parser.add_argument(
-            "-C",
-            "--sort-creation",
-            dest="sort_creation",
-            help="Sort files by date of creation instead of name (default)",
-            action="store_true",
-            default=False)
-        parser.add_argument(
-            "-v",
-            "--verbose",
-            dest="verbose",
-            action="store_true",
-            help="set verbose [default: False]")
+        parser.add_argument("-t", "--title", dest="title", help="Title of the podcast [Defaule:None]",
+                          default=None, metavar="STRING")
+        parser.add_argument("-p", "--description", dest="description", help="Description of the podcast [Defaule:None]",
+                          default=None, metavar="STRING")
+        parser.add_argument("-C", "--sort-creation", dest="sort_creation",
+                          help="Sort files by date of creation instead of name (default)",
+                          action="store_true", default=False)
+        parser.add_argument("-v", "--verbose", dest="verbose", action="store_true",
+                          help="set verbose [default: False]")
         # process options
         opts = parser.parse_args(argv)
 
         if opts.dirname is None or opts.host is None:
-            raise Exception("\n".join([
-                "Usage: python %s -d directory -H hostname [-o output -r]" %
-                (program_name),
-                "For more information run %s --help\n" % (program_name)
-            ]))
+            raise Exception("\n".join(["Usage: python %s -d directory -H hostname [-o output -r]" % (program_name),
+                                       "For more information run %s --help\n" % (program_name)]))
 
         if not os.path.isdir(opts.dirname) or not os.path.exists(opts.dirname):
-            raise Exception(
-                "\n".join["Cannot find directory {0}",
-                          "--direname must be a path to an existing directory".
-                          format(opts.dirname)])
+            raise Exception("\n".join["Cannot find directory {0}",
+                            "--direname must be a path to an existing directory".format(opts.dirname)])
 
         dirname = opts.dirname
         if dirname[-1] != os.sep:
@@ -455,8 +385,7 @@ def main(argv=None):
         if host[-1] != '/':
             host += '/'
 
-        if not host.lower().startswith(
-                "http://") and not host.lower().startswith("https://"):
+        if not host.lower().startswith("http://") and not host.lower().startswith("https://"):
             host = "http://" + host
 
         title = ""
@@ -476,16 +405,10 @@ def main(argv=None):
 
         # get the list of the desired files
         if opts.extensions is not None:
-            opts.extensions = [
-                e for e in opts.extensions.split(",") if e != ""
-            ]
-        fileNames = getFiles(
-            dirname.encode("utf-8"),
-            extensions=opts.extensions,
-            recursive=opts.recursive)
+            opts.extensions = [e for e in  opts.extensions.split(",") if e != ""]
+        fileNames = getFiles(dirname.encode("utf-8"), extensions=opts.extensions, recursive=opts.recursive)
         if len(fileNames) == 0:
-            sys.stderr.write("No media files on directory '%s'\n" %
-                             (opts.dirname))
+            sys.stderr.write("No media files on directory '%s'\n" % (opts.dirname))
             sys.exit(0)
 
         if opts.sort_creation:
@@ -494,7 +417,7 @@ def main(argv=None):
             pubDates = [os.path.getctime(f) for f in fileNames]
             # most feed readers will use pubDate to sort items even if they are not sorted in the output file
             # for readability, we also sort fileNames according to pubDates in the feed.
-            sortedFiles = sorted(zip(fileNames, pubDates), key=lambda f: -f[1])
+            sortedFiles = sorted(zip(fileNames, pubDates),key=lambda f: - f[1])
 
         else:
             # in order to have feed items sorted by name, we give them artificial pubDates
@@ -503,24 +426,17 @@ def main(argv=None):
             # f is a random number of seconds between 0 and 10 (float)
             now = time.time()
             import random
-            pubDates = [
-                now - (60 * 60 * 24 * d + (random.random() * 10))
-                for d in xrange(len(fileNames))
-            ]
+            pubDates = [now - (60 * 60 * 24 * d + (random.random() * 10)) for d in xrange(len(fileNames))]
             sortedFiles = zip(fileNames, pubDates)
 
         # write dates in RFC-822 format
-        sortedFiles = ((f[0],
-                        time.strftime("%a, %d %b %Y %H:%M:%S +0000",
-                                      time.gmtime(f[1]))) for f in sortedFiles)
+        sortedFiles = ((f[0], time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime(f[1]))) for f in sortedFiles)
 
         # build items
-        items = [
-            fileToItem(host, fname, pubDate) for fname, pubDate in sortedFiles
-        ]
+        items = [fileToItem(host, fname, pubDate) for fname, pubDate in sortedFiles]
 
         if opts.outfile is not None:
-            outfp = open(opts.outfile, "w")
+            outfp = open(opts.outfile,"w")
         else:
             outfp = sys.stdout
 
@@ -528,16 +444,14 @@ def main(argv=None):
         outfp.write('<rss version="2.0">\n')
         outfp.write('   <channel>\n')
         outfp.write('      <title>{0}</title>\n'.format(title))
-        outfp.write(
-            '      <description>{0}</description>\n'.format(description))
+        outfp.write('      <description>{0}</description>\n'.format(description))
         outfp.write('      <link>{0}</link>\n'.format(link))
 
         if opts.image is not None:
-            if opts.image.lower().startswith(
-                    "http://") or opts.image.lower().startswith("https://"):
+            if opts.image.lower().startswith("http://") or opts.image.lower().startswith("https://"):
                 imgurl = opts.image
             else:
-                imgurl = urllib.quote(host + opts.image, ":/")
+                imgurl = urllib.quote(host + opts.image,":/")
 
             outfp.write("      <image>\n")
             outfp.write("         <url>{0}</url>\n".format(imgurl))
